b72551a9858f206801896c998affe90a21aa05f5
Chris Moffatt Armes <16cja6@queensu.ca>
2019-01-07 21:06:11 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,26 +1,23 @@
-var cost = document.getElementById("input-cost").value;
-var split = document.getElementById("split").value;
-var service = document.querySelector('input[name="service"]:checked').value;
-var tipValue =
-  [
-    1.10,
-    1.15,
-    1.18,
-    1.20,
-    1.25
-  ];
-
-let totalBill = cost * tipValue[service].toFixed(2);
-let totalTip = (totalBill-cost).toFixed(2);
-let shareTip = (totalBill/split).toFixed(2);
-let shareNoTip = (cost/split).toFixed(2);
-let tipShare = ((totalBill-cost)/split).toFixed(2);
-
-
 function tipCalc() {
-    document.getElementById("output").innerHTML = (cost * tipValue[service]);
-    // "Based on your input, you should tip "+totalTip+", bringing your total bill to "+totalBill+".";
-    if(split == 1) {
-      document.getElementById("share").innerHTML = "Each person's share of the bill is "+share+": "+shareNoTip+" for the bill, and "+tipShare+" for the tip."
-    }
-  };
+	var cost = document.getElementById("input-cost").value;
+	var split = document.getElementById("split").value;
+	var service = document.querySelector('input[name="service"]:checked').value;
+	var tipValue = [
+		1.10,
+		1.15,
+		1.18,
+		1.20,
+		1.25
+	];
+
+	let totalBill = cost * tipValue[service].toFixed(2);
+	let totalTip = (totalBill - cost).toFixed(2);
+	let shareTip = (totalBill / split).toFixed(2);
+	let shareNoTip = (cost / split).toFixed(2);
+	let tipShare = ((totalBill - cost) / split).toFixed(2);
+  if (split == 1) {
+		document.getElementById("share").innerHTML = "Each person's share of the bill is " + share + ": " + shareNoTip + " for the bill, and " + tipShare + " for the tip."
+	};
+	document.getElementById("output").innerHTML = "Based on your input, you should tip "+totalTip+", bringing your total bill to "+totalBill+"."
+
+};
